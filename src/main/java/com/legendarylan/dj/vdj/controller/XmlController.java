9359adf05dc9b874d622a4090d3eca790e8cc97f
Aaron Saraco <sirlemming@gmail.com>
2024-09-15 18:42:37 -0400

    First implementation of Play History

--- src/main/java/com/legendarylan/dj/vdj/controller/XmlController.java
+++ src/main/java/com/legendarylan/dj/vdj/controller/XmlController.java
@@ -2,6 +2,10 @@ package com.legendarylan.dj.vdj.controller;
 
 import com.legendarylan.dj.vdj.data.*;
 import jakarta.annotation.PostConstruct;
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.Cacheable;
@@ -12,21 +16,43 @@ import org.springframework.xml.transform.StringSource;
 
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @CrossOrigin({"http://${app.legendarydj.localhost-ip}:8080", "http://${app.legendarydj.localhost-ip}:4200", "http://localhost:4200"})
 public class XmlController {
+    private static Logger logger = LogManager.getLogger(XmlController.class);
     @Autowired
     private Jaxb2Marshaller marshaller;
 
     private static List<Track> allTracks = null;
 
     private static File xmlDatabase = new File("C:\\Users\\lemmh\\AppData\\Local\\VirtualDJ\\database.xml");
-    private static File automixQueue = new File("C:\\Users\\lemmh\\AppData\\Local\\VirtualDJ\\Sideview\\automix.vdjfolder");;
+    private static File automixQueue = new File("C:\\Users\\lemmh\\AppData\\Local\\VirtualDJ\\Sideview\\automix.vdjfolder");
+    private static File historyPath = new File("C:\\Users\\lemmh\\AppData\\Local\\VirtualDJ\\History\\");
+    private static File historyPlaylistFile;
+
+    static {
+        if (historyPath.isDirectory()) {
+            File[] dirFiles = historyPath.listFiles((FileFilter) FileFilterUtils.fileFileFilter());
+            if (dirFiles!=null && dirFiles.length>0) {
+                Arrays.sort(dirFiles, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+                historyPlaylistFile = dirFiles[0];
+                logger.debug(historyPlaylistFile);
+            }
+        }
+    }
 
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
@@ -84,6 +110,37 @@ public class XmlController {
         VirtualFolder vdjfolder = (VirtualFolder) marshaller.unmarshal(new StreamSource(automixQueue));
         p.setName("Automix Queue");
         p.setPlaylistTracks(vdjfolder.getSongs());
+        return p;
+    }
+
+    @GetMapping("/getPlayHistory")
+    public Playlist getPlayHistory() throws IOException {
+        Playlist p = new Playlist();
+        List<String> fileLines = Files.readAllLines(Paths.get(historyPlaylistFile.toURI()));
+        //logger.debug(fileLines);
+        List<PlaylistSong> pSongs = new ArrayList<>();
+        PlaylistSong currentPS = new PlaylistSong();
+        for (String line : fileLines) {
+            if (line.startsWith("#EXTVDJ:")) {
+                //logger.debug("EXTVDJ - " + line);
+                PlaylistSong ps = new PlaylistSong();
+                int iArtistStart = line.indexOf("<artist>");
+                int iArtistEnd = line.indexOf("</artist");
+                String sArtist = line.substring(iArtistStart, iArtistEnd);
+                int iTitleStart = line.indexOf("<title>");
+                int iTitleEnd = line.indexOf("</title>");
+                String sTitle = line.substring(iTitleStart, iTitleEnd);
+                ps.setArtist(sArtist);
+                ps.setTitle(sTitle);
+                currentPS = ps;
+            } else {
+                //logger.debug("NOT EXTVDJ - " + line);
+                currentPS.setPath(line);
+                pSongs.add(currentPS);
+            }
+        }
+        p.setName("Last Played");
+        p.setPlaylistTracks(pSongs);
         return p;
     }
 }
